4e853d5f98df71b882716ef35207feadcd321cc4
antiqueMouse <s.yakovlev06@yahoo.com>
2025-12-10 10:55:23 +0300

    fix 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   base: "/Star-Analyst/",
@@ -12,46 +11,38 @@ export default defineConfig({
       registerType: "autoUpdate",
       devOptions: {
         enabled: true,
-        type: "module",
       },
       manifest: {
-        name: "Star Analyst",
-        short_name: "Star Analyst",
-        start_url: "/Star-Analyst/",
-        scope: "/Star-Analyst/",
+        name: "Звёздный Аналитик",
+        short_name: "Аналитик",
+        description: "Исследуй спектральные классы звёзд",
+        theme_color: "#000000",
+        background_color: "#000000",
         display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#0072ce",
-        description: "Калькулятор масс звезд",
         icons: [
           {
-            src: "/pwa-192.png",
+            src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any",
           },
           {
-            src: "/pwa-512.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any",
           },
         ],
       },
     }),
-    mkcert(),
   ],
-
   server: {
     port: 3000,
+    open: true,
     proxy: {
       "/api": {
         target: "http://localhost:8080",
         changeOrigin: true,
-      },
-      "/devices": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
